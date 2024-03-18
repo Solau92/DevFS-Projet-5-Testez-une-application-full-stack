fd2c35d66d8b23f38e3c3a73396ee66bb2003df1
Sophie LAURENT <sophielaurent92@gmail.com>
2024-03-18 09:42:02 +0100

    test e2e create session ok

--- front/cypress/e2e/detail.cy.ts
+++ front/cypress/e2e/detail.cy.ts
@@ -61,100 +61,100 @@ describe('Detail tests', () => {
 
         // Affichage d'une session (click on "Detail")
 
-        cy.intercept('GET', 'api/session/detail/1', {
+        // **************** TODO ****************
+        
+        cy.intercept('GET', '/api/session/1', {
             body: session1
         })
-
-        cy.intercept('GET', '/api/session/1', session1)
-        cy.intercept('GET', '/api/teacher/1', teacher1)
+        cy.intercept('GET', '/api/teacher/1', {
+            body: teacher1
+        })
 
         cy.contains('Detail').click()
 
         // Verify 
         // Display + delete is notpossible + participate / not is possible
 
-        cy.url().should('include', 'sessions/detail')
+        cy.url().should('include', 'session/detail')
         cy.contains('attendees')
         cy.contains('session1')
         cy.get('button').should('be.visible')
 
     });
 
-    it('should display the details of a session for admin', () => {
+    // it('should display the details of a session for admin', () => {
 
-        cy.visit('/login')
+    //     cy.visit('/login')
 
-        cy.intercept('POST', '/api/auth/login', {
-            body: {
-                id: 1,
-                username: 'userName2',
-                firstName: 'firstName2',
-                lastName: 'lastName2',
-                admin: true
-            },
-        })
+    //     cy.intercept('POST', '/api/auth/login', {
+    //         body: {
+    //             id: 1,
+    //             username: 'userName2',
+    //             firstName: 'firstName2',
+    //             lastName: 'lastName2',
+    //             admin: true
+    //         },
+    //     })
 
-        // Affichage des sessions 
+    //     // Affichage des sessions 
 
-        const teacher1 = {
-            id: 1,
-            lastName: "teacher1LastName",
-            firstName: "teacher2FirstName",
-            createdAt: new Date(),
-            updatedAt: new Date()
-        }
+    //     const teacher1 = {
+    //         id: 1,
+    //         lastName: "teacher1LastName",
+    //         firstName: "teacher2FirstName",
+    //         createdAt: new Date(),
+    //         updatedAt: new Date()
+    //     }
 
-        const session1 = {
-            id: 1,
-            name: "session1",
-            date: "2024-04-05T08:00:00.000+00:00",
-            teacher_id: 1,
-            description: "session 1",
-            users: [],
-            createdAt: "2024-03-15T08:23:02",
-            updatedAt: "2024-03-15T08:23:02"
-        }
+    //     const session1 = {
+    //         id: 1,
+    //         name: "session1",
+    //         date: "2024-04-05T08:00:00.000+00:00",
+    //         teacher_id: 1,
+    //         description: "session 1",
+    //         users: [],
+    //         createdAt: "2024-03-15T08:23:02",
+    //         updatedAt: "2024-03-15T08:23:02"
+    //     }
 
-        const session2 = {
-            id: 2,
-            name: "session2",
-            date: "2024-04-05T08:00:00.000+00:00",
-            teacher_id: 1,
-            description: "session 2",
-            users: [],
-            createdAt: "2024-03-15T08:23:02",
-            updatedAt: "2024-03-15T08:23:02"
-        }
+    //     const session2 = {
+    //         id: 2,
+    //         name: "session2",
+    //         date: "2024-04-05T08:00:00.000+00:00",
+    //         teacher_id: 1,
+    //         description: "session 2",
+    //         users: [],
+    //         createdAt: "2024-03-15T08:23:02",
+    //         updatedAt: "2024-03-15T08:23:02"
+    //     }
 
-        const sessions = [{ session1 }, { session2 }]
+    //     const sessions = [{ session1 }, { session2 }]
 
-        cy.intercept('GET', '/api/session', {
-            body: sessions
-        })
+    //     cy.intercept('GET', '/api/session', {
+    //         body: sessions
+    //     })
 
-        cy.get('input[formControlName=email]').type("yoga@studio.com")
-        cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+    //     cy.get('input[formControlName=email]').type("yoga@studio.com")
+    //     cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
 
-        // Click on "Detail"
+    //     // Click on "Detail"
 
-        cy.intercept('GET', '/session/detail/1', {
-            body: session1
-        })
+    //     cy.intercept('GET', '/session/detail/1', {
+    //         body: session1
+    //     })
 
-        cy.intercept('GET', '/api/session/1', session1)
-        cy.intercept('GET', '/api/teacher/1', teacher1)
+    //     cy.intercept('GET', '/api/session/1', session1)
+    //     cy.intercept('GET', '/api/teacher/1', teacher1)
 
-        cy.contains('Detail').click()
+    //     cy.contains('Detail').click()
 
-        // Verify 
-        // Display + delete is possible + participate / not impossible
-        cy.url().should('include', 'sessions/detail')
-        cy.contains('attendees')
-        cy.contains('session1')
-        cy.get('button').should('be.visible')
+    //     // Verify 
+    //     // Display + delete is possible + participate / not impossible
+    //     cy.url().should('include', 'sessions/detail')
+    //     cy.contains('attendees')
+    //     cy.contains('session1')
+    //     cy.get('button').should('be.visible')
 
-    });
-
-
+    // });
 
 });
--- front/cypress/e2e/form.cy.ts
+++ front/cypress/e2e/form.cy.ts
@@ -1,150 +1,147 @@
 describe('List tests', () => {
 
-    // it('should create a session (admin)', () => {
+    it('should create a session (admin)', () => {
 
-    //     // Admin login
+        // Admin login
 
-    //     cy.visit('/login')
+        cy.visit('/login')
 
-    //     cy.intercept('POST', '/api/auth/login', {
-    //         body: {
-    //             id: 1,
-    //             username: 'userName2',
-    //             firstName: 'firstName2',
-    //             lastName: 'lastName2',
-    //             admin: true
-    //         },
-    //     })
+        cy.intercept('POST', '/api/auth/login', {
+            body: {
+                id: 1,
+                username: 'userName2',
+                firstName: 'firstName2',
+                lastName: 'lastName2',
+                admin: true
+            },
+        })
 
-    //     // Affichage des sessions 
+        // Affichage des sessions 
 
-    //     const sessions = [{
-    //         id: 1,
-    //         name: "session1",
-    //         date: "2024-04-05T08:00:00.000+00:00",
-    //         teacher_id: 1,
-    //         description: "session 1",
-    //         users: [],
-    //         createdAt: "2024-03-15T08:23:02",
-    //         updatedAt: "2024-03-15T08:23:02"
-    //     },
-    //     {
-    //         id: 2,
-    //         name: "session2",
-    //         date: "2024-04-05T08:00:00.000+00:00",
-    //         teacher_id: 1,
-    //         description: "session 2",
-    //         users: [],
-    //         createdAt: "2024-03-15T08:23:02",
-    //         updatedAt: "2024-03-15T08:23:02"
-    //     }]
+        const sessions = [{
+            id: 1,
+            name: "session1",
+            date: "2024-04-05T08:00:00.000+00:00",
+            teacher_id: 1,
+            description: "session 1",
+            users: [],
+            createdAt: "2024-03-15T08:23:02",
+            updatedAt: "2024-03-15T08:23:02"
+        },
+        {
+            id: 2,
+            name: "session2",
+            date: "2024-04-05T08:00:00.000+00:00",
+            teacher_id: 1,
+            description: "session 2",
+            users: [],
+            createdAt: "2024-03-15T08:23:02",
+            updatedAt: "2024-03-15T08:23:02"
+        }]
 
-    //     cy.intercept('GET', '/api/session', {
-    //         body: sessions
-    //     })
+        cy.intercept('GET', '/api/session', {
+            body: sessions
+        })
 
-    //     cy.get('input[formControlName=email]').type("yoga@studio.com")
-    //     cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+        cy.get('input[formControlName=email]').type("yoga@studio.com")
+        cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
 
-    //     // Click on "Create"
+        // Click on "Create"
 
-    //     const teachers = [{
-    //         id: 1,
-    //         lastName: 'teacher1LastName',
-    //         firstName: 'teacher1FirstName',
-    //         createdAt: new Date(),
-    //         updatedAt: new Date()
-    //     },
-    //     {
-    //         id: 2,
-    //         lastName: 'teacher2LastName',
-    //         firstName: 'teacher2FirstName',
-    //         createdAt: new Date(),
-    //         updatedAt: new Date()
-    //     }
-    //     ]
+        const teachers = [{
+            id: 1,
+            lastName: 'teacher1LastName',
+            firstName: 'teacher1FirstName',
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            id: 2,
+            lastName: 'teacher2LastName',
+            firstName: 'teacher2FirstName',
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }
+        ]
 
-    //     cy.intercept('GET', '/api/teacher', teachers)
-    //     cy.intercept('GET', '/api/session/create')
+        cy.intercept('GET', '/api/teacher', teachers)
+        //cy.intercept('GET', '/api/session/create')
 
-    //     cy.contains('Create').click()
+        cy.contains('Create').click()
 
-    //     // Verify (affichage form)
+        // Verify (affichage form)
 
-    //     cy.contains('Create session')
-    //     cy.get('button[type="submit"]').should('be.disabled')
+        cy.contains('Create session')
+        cy.get('button[type="submit"]').should('be.disabled')
 
-    //     // Remplissage des champs 
+        // Remplissage des champs 
 
-    //     cy.get('input[formControlName="name"]').type("my yoga session")
-    //     cy.get('input[formControlName="date"]').type("2024-03-18")
-    //     cy.get('mat-select[formControlName="teacher_id"]')
-    //         .first().click()
-    //         .get('mat-option').contains('1')
-    //         .click();
-    //     cy.get('textarea[formControlName="description"]').type("this is a new yoga session")
+        cy.get('input[formControlName="name"]').type("my yoga session")
+        cy.get('input[formControlName="date"]').type("2024-03-18")
+        cy.get('mat-select[formControlName="teacher_id"]')
+            .first().click()
+            .get('mat-option').contains('1')
+            .click();
+        cy.get('textarea[formControlName="description"]').type("this is a new yoga session")
 
-    //     // Verify (affichage form)
+        // Verify (affichage form)
 
-    //     cy.get('button[type="submit"]').should('not.be.disabled')
+        cy.get('button[type="submit"]').should('not.be.disabled')
 
-    //     // Click on save 
+        // Click on save 
 
-    //     cy.intercept('POST', '/api/session'), {
-    //         body: {
-    //             name: "my yoga session",
-    //             date: "2024-03-18",
-    //             teacher_id: 1,
-    //             description: "this is a new yoga session"
-    //         }
-    //     }
+        cy.intercept('POST', '/api/session', {
+            body: {
+                name: "my yoga session",
+                date: "2024-03-18",
+                teacher_id: 1,
+                description: "this is a new yoga session"
+            }
+        })
 
-    //     const sessionsUpdated = [{
-    //         id: 1,
-    //         name: "session1",
-    //         date: "2024-04-05T08:00:00.000+00:00",
-    //         teacher_id: 1,
-    //         description: "session 1",
-    //         users: [],
-    //         createdAt: "2024-03-15T08:23:02",
-    //         updatedAt: "2024-03-15T08:23:02"
-    //     },
-    //     {
-    //         id: 2,
-    //         name: "session2",
-    //         date: "2024-04-05T08:00:00.000+00:00",
-    //         teacher_id: 1,
-    //         description: "session 2",
-    //         users: [],
-    //         createdAt: "2024-03-15T08:23:02",
-    //         updatedAt: "2024-03-15T08:23:02"
-    //     },
-    //     {
-    //         id: 3,
-    //         name: "my yoga session",
-    //         date: "2024-03-18T08:00:00.000+00:00",
-    //         teacher_id: 1,
-    //         description: "this is a new yoga session",
-    //         users: [],
-    //         createdAt: new Date(),
-    //         updatedAt: new Date()
-    //     }]
+        const sessionsUpdated = [{
+            id: 1,
+            name: "session1",
+            date: "2024-04-05T08:00:00.000+00:00",
+            teacher_id: 1,
+            description: "session 1",
+            users: [],
+            createdAt: "2024-03-15T08:23:02",
+            updatedAt: "2024-03-15T08:23:02"
+        },
+        {
+            id: 2,
+            name: "session2",
+            date: "2024-04-05T08:00:00.000+00:00",
+            teacher_id: 1,
+            description: "session 2",
+            users: [],
+            createdAt: "2024-03-15T08:23:02",
+            updatedAt: "2024-03-15T08:23:02"
+        },
+        {
+            id: 3,
+            name: "my yoga session",
+            date: "2024-03-18T08:00:00.000+00:00",
+            teacher_id: 1,
+            description: "this is a new yoga session",
+            users: [],
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }]
 
-    //     cy.intercept('GET', '/api/session', {
-    //         body: sessionsUpdated
-    //     })
+        cy.intercept('GET', '/api/session', {
+            body: sessionsUpdated
+        })
 
-    //     cy.get('button[type="submit"]').click()
-    //     //cy.contains('Save').click()
+        cy.contains('Save').click()
 
-    //     // TODO : voir pourquoi fait pas la redirection... 
+        // Verify (affichage de la nouvelle session sur la page list 
 
-    //     // Verify (affichage de la nouvelle session sur la page list 
+        cy.url().should('include', 'sessions')
+        cy.contains('this is a new yoga session')
 
-    //     cy.url().should('include', 'sessions')
-    //     cy.contains('this is a new yoga session')
-
-    // });
+    });
 
 
     it('should update a session (admin)', () => {
@@ -214,12 +211,80 @@ describe('List tests', () => {
 
         cy.contains('Edit').click()
 
-        // TODO : voir pourquoi marche pas... session/undefined ??
+        // **************** TODO ****************
 
         // Verify (affichage form)
 
         cy.contains('Update session')
-        cy.get('button[type="submit"]').should('be.disabled')
+        // TODO : voir si je vérifie autre chose 
+
+        // Modification des champs 
+
+        // TODO : voir si faut .clear() avant de modifier ? 
+        cy.get('input[formControlName="name"]').type("my yoga session updated")
+        cy.get('input[formControlName="date"]').type("2024-03-18")
+        cy.get('mat-select[formControlName="teacher_id"]')
+            .first().click()
+            .get('mat-option').contains('1')
+            .click();
+        cy.get('textarea[formControlName="description"]').type("this is a new yoga session updated")
+
+        // Verify (affichage form)
+
+        cy.get('button[type="submit"]').should('not.be.disabled')
+
+        // Click on save 
+
+        cy.intercept('POST', '/api/session', {
+            body: {
+                name: "my yoga session updated",
+                date: "2024-03-18",
+                teacher_id: 1,
+                description: "this is a new yoga session updated"
+            }
+        })
+
+        const sessionsUpdated = [{
+            id: 1,
+            name: "session1",
+            date: "2024-04-05T08:00:00.000+00:00",
+            teacher_id: 1,
+            description: "session 1",
+            users: [],
+            createdAt: "2024-03-15T08:23:02",
+            updatedAt: "2024-03-15T08:23:02"
+        },
+        {
+            id: 2,
+            name: "session2",
+            date: "2024-04-05T08:00:00.000+00:00",
+            teacher_id: 1,
+            description: "session 2",
+            users: [],
+            createdAt: "2024-03-15T08:23:02",
+            updatedAt: "2024-03-15T08:23:02"
+        },
+        {
+            id: 3,
+            name: "my yoga session updated",
+            date: "2024-03-18T08:00:00.000+00:00",
+            teacher_id: 1,
+            description: "this is a new yoga session updated",
+            users: [],
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }]
+
+        cy.intercept('GET', '/api/session', {
+            body: sessionsUpdated
+        })
+
+        cy.contains('Save').click()
+
+        // Verify (affichage de la nouvelle session sur la page list 
+
+        cy.url().should('include', 'sessions')
+        cy.contains('this is a new yoga session updated')
 
     });
 });
